dbecb7446fa36b1c49bc730ba5da11b0fb3dc437
Yuntrax <javi.marrugo@gmail.com>
2021-03-15 09:21:59 -0500

    Registro

--- app/src/main/java/com/example/appvacov/MainActivity.java
+++ app/src/main/java/com/example/appvacov/MainActivity.java
@@ -6,22 +6,87 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    Button registrarse;
+    Button btn1,btn2;
+    EditText editText1,editText2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        registrarse = (Button)findViewById(R.id.button2);
-        registrarse.setOnClickListener(new View.OnClickListener() {
+        editText1 = (EditText)findViewById(R.id.editText1);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        btn1 = (Button) findViewById(R.id.button1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent registrarse = new Intent(MainActivity.this,Registro.class);
-                startActivity(registrarse);
+                validar_usuario("http://192.168.0.227/appvacov/login.php");
+
+                
             }
         });
 
+         btn2 = (Button) findViewById(R.id.button2);
+         btn2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this,Registro.class));
+        }
+    });
+
+
     }
+    private void validar_usuario(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>(){
+            @Override
+            public void onResponse (String Response){
+
+                if(!Response.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Log In", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Usuario o Clave Incorrecto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse (VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("cedula", editText1.getText().toString());
+                parametros.put("clave", editText2.getText().toString());
+                return parametros;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
 }
